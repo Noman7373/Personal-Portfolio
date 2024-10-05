d021dd5b05bd5b158e58f68ae3672d9a1d40afe8
NomanAhmed <officialnoman515@gmail.com>
2024-10-05 22:32:06 +0300

    Added project link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,10 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Project = () => {
   return (
-    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20  dark:text-white dark:bg-black">
+    <div
+      id="work"
+      className="w-full px-[12%] py-10 scroll-mt-20  dark:text-white dark:bg-black"
+    >
       <h4 className="text-center mb-2 text-lg font-ovo ">My portfolio</h4>
       <h2 className="text-center text-5xl font-ovo">My latest work</h2>
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12font-ovo">
@@ -11,25 +14,32 @@ const Project = () => {
         projects showcasing my expertise in front-end development.
       </p>
       <div className="grid grid-cols-auto my-10 gap-5">
-        <div className="aspect-square bg-[url(/images/work-1.png)] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
-          <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h2 className="font-semibold dark:text-black">Frontend project</h2>
-              <p className="text-sm text-gray-700">Web design</p>
-            </div>
-            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow=[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-              <img
-                src="/images/send-icon.png"
-                alt="send-icon"
-                className="w-5"
-              />
+        <a href="https://fintrackexpensetrackhq.netlify.app/" target="_blank">
+          <div className="aspect-square bg-[url(/images/project1.jpg)] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold dark:text-black">
+                  Frontend project
+                </h2>
+                <p className="text-sm text-gray-700">Web design</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow=[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <img
+                  src="/images/send-icon.png"
+                  alt="send-icon"
+                  className="w-5"
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </a>
+
         <div className="aspect-square bg-[url(/images/work-2.png)] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
           <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
             <div>
-              <h2 className="font-semibold dark:text-black">Frontend project</h2>
+              <h2 className="font-semibold dark:text-black">
+                Frontend project
+              </h2>
               <p className="text-sm text-gray-700">Web design</p>
             </div>
             <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow=[2px_2px_0_#000] group-hover:bg-lime-300 transition">
@@ -44,7 +54,9 @@ const Project = () => {
         <div className="aspect-square bg-[url(/images/work-3.png)] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
           <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
             <div>
-              <h2 className="font-semibold dark:text-black">Crypto Dashboard</h2>
+              <h2 className="font-semibold dark:text-black">
+                Crypto Dashboard
+              </h2>
               <p className="text-sm text-gray-700">
                 I used Authentication on this project
               </p>
